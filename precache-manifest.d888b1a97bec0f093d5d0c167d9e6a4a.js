f9448c467692d8289ce2271c8d0454d32dd501c2
J.VELAYUTHAM <jvandco30@gmail.com>
2022-09-01 08:27:31 +0530

    Updates

---
+++ precache-manifest.d888b1a97bec0f093d5d0c167d9e6a4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3567b0074df1c458343c",
+    "url": "/react-gh-pages/static/css/main.cf113e10.chunk.css"
+  },
+  {
+    "revision": "3567b0074df1c458343c",
+    "url": "/react-gh-pages/static/js/main.3567b007.chunk.js"
+  },
+  {
+    "revision": "0d630a3e1f21db7817dc",
+    "url": "/react-gh-pages/static/js/1.0d630a3e.chunk.js"
+  },
+  {
+    "revision": "3f74f14483aa6d119cd4",
+    "url": "/react-gh-pages/static/js/2.3f74f144.chunk.js"
+  },
+  {
+    "revision": "125fed160038593e3dde",
+    "url": "/react-gh-pages/static/js/runtime~main.125fed16.js"
+  },
+  {
+    "revision": "22a19302ab8f1116c62af9df98cafba3",
+    "url": "/react-gh-pages/index.html"
+  }
+];
